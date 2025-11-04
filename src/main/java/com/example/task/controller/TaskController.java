f00c9ec53e5d09e48a1d9658278abcf10b242164
loic-job-dev <loicjobard@hotmail.com>
2025-11-04 11:01:16 +0100

    fix : tests for TaskService in the good file

--- src/main/java/com/example/task/controller/TaskController.java
+++ src/main/java/com/example/task/controller/TaskController.java
@@ -1,22 +1,33 @@
 package com.example.task.controller;
 
+import com.example.task.Task;
 import com.example.task.service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/tasks")
-public class TaskController {
+public class TaskController<T> {
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+
+        //Add some random tasks to easy tests
+        taskService.addTask(new Task("pippo"));
+        taskService.addTask(new Task("foo"));
+        taskService.addTask(new Task("tizzio"));
     }
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Welcome to the Task Manager API!");
     }
+
+    @GetMapping()
+    public ResponseEntity<List<T>> getAllTasks() { return ResponseEntity.ok((List<T>) taskService.getTaskList());}
 }
---
+++ src/test/java/com/example/task/TaskServiceTests.java
@@ -0,0 +1,55 @@
+package com.example.task;
+
+import com.example.task.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskServiceTests {
+    @Test
+    void testEmptyListByDefault() {
+        TaskService taskService = new TaskService();
+
+        assertTrue(taskService.getTaskList().isEmpty());
+    }
+
+    @Test
+    void testAddTask(){
+        TaskService taskService = new TaskService();
+
+        Task task1 = new Task("pippo");
+        taskService.addTask(task1);
+
+        assertFalse(taskService.getTaskList().isEmpty());
+        assertSame(task1, taskService.getTaskList().get(0));
+    }
+
+    @Test
+    void testDeleteTask(){
+        TaskService taskService = new TaskService();
+        Task task1 = new Task("pippo");
+        taskService.addTask(task1);
+        Task task2 = new Task("foo");
+        taskService.addTask(task2);
+
+        assertEquals(2, taskService.getTaskList().size());
+
+        taskService.deleteTask(task1.getId());
+
+        assertEquals(1, taskService.getTaskList().size());
+        assertSame(task2, taskService.getTaskList().get(0));
+    }
+
+    @Test
+    void testTaskStateIsChanged() {
+        TaskService taskService = new TaskService();
+        Task task1 = new Task("pippo");
+        taskService.addTask(task1);
+
+        assertFalse(taskService.getTaskList().get(0).isOver());
+
+        taskService.checkTask(taskService.getTaskList().get(0).getId());
+
+        assertTrue(taskService.getTaskList().get(0).isOver());
+    }
+}
